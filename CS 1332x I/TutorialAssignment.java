62e740814a3d9c85cbdb1c698477a75d8472b3a9
Mario Fernandez <diddleslip@gmail.com>
2024-01-30 13:11:28 -0500

    Assigned 4 class folders and created and successfully ran TutorialAssignment!

---
+++ CS 1332x I/TutorialAssignment.java
@@ -0,0 +1,35 @@
+public class TutorialAssignment {
+
+  /**
+   * This method should return the largest number in the passed-in array.
+   *
+   * Constraints:
+   * 1 <= nums.length <= 100
+   * -10000 <= nums[i] <= 10000
+   *
+   * @param nums An array containing integers.
+   * @return The largest number in nums.
+   */
+  public static int tutorialAssignment(int[] nums) {
+    // Write your solution here.
+    int maxNum = Integer.MIN_VALUE;
+
+    for (int i : nums) {
+      if (i > maxNum) {
+        maxNum = i;
+      }
+    }
+    return maxNum;
+  }
+
+  public static void main(String[] args) {
+    // Create an instance of the class
+    TutorialAssignment one = new TutorialAssignment();
+
+    // Call the method on the instance
+    int[] array = {1, 2, 3, 4, 5};
+
+    // Print the result
+    System.out.println(one.tutorialAssignment(array));
+  }
+}
