221a635402229c20dd0bfcd07d5c7068a004dbab
Mario Fernandez <diddleslip@gmail.com>
2024-02-21 12:14:20 -0500

    Instanced pm function

--- I/Module 1/TowersOfHanoi.java
+++ I/Module 1/TowersOfHanoi.java
@@ -1,5 +1,9 @@
 public class TowersOfHanoi {
   public static void main(String[] args) {
-    
+
+  }
+
+  private static String pm(String start, String end ) {
+    System.out.println(start + "->" + end);
   }
 }
