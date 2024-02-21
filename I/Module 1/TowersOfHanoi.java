4c8fc3a10b6ed66a75cf79e47c9eea2c7d8aa225
Mario Fernandez <diddleslip@gmail.com>
2024-02-21 12:27:46 -0500

    Added hanoi function and implemeted it; Fully working!

--- I/Module 1/TowersOfHanoi.java
+++ I/Module 1/TowersOfHanoi.java
@@ -1,9 +1,21 @@
 public class TowersOfHanoi {
   public static void main(String[] args) {
-
+    hanoi(3, 1, 3);
+  }
+  private static void hanoi(int n, int start, int end) {
+    // Base case
+    if (n == 1) {
+      pm(start, end);
+    }
+    else {
+      int other = 6 - (start + end);
+      hanoi(n - 1, start, other);
+      pm(start, end);
+      hanoi(n - 1, other, end);
+    }
   }
 
-  private static String pm(String start, String end ) {
+  private static void pm(int start, int end ) {
     System.out.println(start + "->" + end);
   }
 }
