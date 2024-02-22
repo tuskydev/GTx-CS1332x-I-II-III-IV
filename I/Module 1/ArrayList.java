741727e95d14bd57533ae49af97cb51ab849036b
Mario Fernandez <diddleslip@gmail.com>
2024-02-22 12:28:12 -0500

    Taking break for solution solving

--- I/Module 1/ArrayList.java
+++ I/Module 1/ArrayList.java
@@ -5,109 +5,120 @@ import java.util.NoSuchElementException;
  */
 public class ArrayList<T> {
 
-    /*
-     * The initial capacity of the ArrayList.
-     *
-     * DO NOT MODIFY THIS VARIABLE!
-     */
-    public static final int INITIAL_CAPACITY = 9;
+  /*
+    * The initial capacity of the ArrayList.
+    *
+    * DO NOT MODIFY THIS VARIABLE!
+    */
+  public static final int INITIAL_CAPACITY = 9;
 
-    /*
-     * Do not add new instance variables or modify existing ones.
-     */
-    private T[] backingArray;
-    private int size;
+  /*
+    * Do not add new instance variables or modify existing ones.
+    */
+  private T[] backingArray;
+  private int size;
 
-    /**
-     * This is the constructor that constructs a new ArrayList.
-     * 
-     * Recall that Java does not allow for regular generic array creation,
-     * so instead we cast an Object[] to a T[] to get the generic typing.
-     */
-    public ArrayList() {
-        //DO NOT MODIFY THIS METHOD!
-        backingArray = (T[]) new Object[INITIAL_CAPACITY];
+  /**
+   * This is the constructor that constructs a new ArrayList.
+   *
+   * Recall that Java does not allow for regular generic array creation,
+   * so instead we cast an Object[] to a T[] to get the generic typing.
+   */
+  public ArrayList() {
+    //DO NOT MODIFY THIS METHOD!
+    backingArray = (T[]) new Object[INITIAL_CAPACITY];
+  }
+
+  /**
+   * Adds the data to the front of the list.
+   *
+   * This add may require elements to be shifted.
+   *
+   * Method should run in O(n) time.
+   *
+   * @param data the data to add to the front of the list
+   * @throws java.lang.IllegalArgumentException if data is null
+   */
+  public void addToFront(T data) {
+    // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    if (data == null) {
+      throw new java.lang.IllegalArgumentException();
+    } else {
+      size += 1;
+
+      // Make new backingArray with doubled size
+      if(size == INITIAL_CAPACITY) {
+        // size *= 2;
+      }
+
     }
+  }
 
-    /**
-     * Adds the data to the front of the list.
-     *
-     * This add may require elements to be shifted.
-     *
-     * Method should run in O(n) time.
-     *
-     * @param data the data to add to the front of the list
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
-    public void addToFront(T data) {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+  /**
+   * Adds the data to the back of the list.
+   *
+   * Method should run in amortized O(1) time.
+   *
+   * @param data the data to add to the back of the list
+   * @throws java.lang.IllegalArgumentException if data is null
+   */
+  public void addToBack(T data) {
+    // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+  }
 
-    /**
-     * Adds the data to the back of the list.
-     *
-     * Method should run in amortized O(1) time.
-     *
-     * @param data the data to add to the back of the list
-     * @throws java.lang.IllegalArgumentException if data is null
-     */
-    public void addToBack(T data) {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+  /**
+   * Removes and returns the first data of the list.
+   *
+   * Do not shrink the backing array.
+   *
+   * This remove may require elements to be shifted.
+   *
+   * Method should run in O(n) time.
+   *
+   * @return the data formerly located at the front of the list
+   * @throws java.util.NoSuchElementException if the list is empty
+   */
+  public T removeFromFront() {
+    // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+  }
 
-    /**
-     * Removes and returns the first data of the list.
-     *
-     * Do not shrink the backing array.
-     *
-     * This remove may require elements to be shifted.
-     *
-     * Method should run in O(n) time.
-     *
-     * @return the data formerly located at the front of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
-    public T removeFromFront() {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+  /**
+   * Removes and returns the last data of the list.
+   *
+   * Do not shrink the backing array.
+   *
+   * Method should run in O(1) time.
+   *
+   * @return the data formerly located at the back of the list
+   * @throws java.util.NoSuchElementException if the list is empty
+   */
+  public T removeFromBack() {
+    // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+  }
 
-    /**
-     * Removes and returns the last data of the list.
-     *
-     * Do not shrink the backing array.
-     *
-     * Method should run in O(1) time.
-     *
-     * @return the data formerly located at the back of the list
-     * @throws java.util.NoSuchElementException if the list is empty
-     */
-    public T removeFromBack() {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
-    }
+  /**
+   * Returns the backing array of the list.
+   *
+   * For grading purposes only. You shouldn't need to use this method since
+   * you have direct access to the variable.
+   *
+   * @return the backing array of the list
+   */
+  public T[] getBackingArray() {
+    // DO NOT MODIFY THIS METHOD!
+    return backingArray;
+  }
 
-    /**
-     * Returns the backing array of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the backing array of the list
-     */
-    public T[] getBackingArray() {
-        // DO NOT MODIFY THIS METHOD!
-        return backingArray;
-    }
-
-    /**
-     * Returns the size of the list.
-     *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
-     * @return the size of the list
-     */
-    public int size() {
-        // DO NOT MODIFY THIS METHOD!
-        return size;
-    }
+  /**
+   * Returns the size of the list.
+   *
+   * For grading purposes only. You shouldn't need to use this method since
+   * you have direct access to the variable.
+   *
+   * @return the size of the list
+   */
+  public int size() {
+    // DO NOT MODIFY THIS METHOD!
+    return size;
+  }
 }
