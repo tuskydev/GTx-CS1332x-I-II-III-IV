12942cc70d733ba302a6fb79a9fd99a535bb55be
Mario Fernandez <diddleslip@gmail.com>
2024-03-07 13:37:31 -0500

    .

--- I/Module Exam/SinglyLinkedList.java
+++ I/Module Exam/SinglyLinkedList.java
@@ -18,7 +18,7 @@ public class SinglyLinkedList<T> {
    * Adds the element to the specified index.
    *
    * Must be O(1) for indices 0 and size and O(n) for all other cases.
-   * 
+   *
    * ASSUMPTIONS:
    * - You may assume that the index will always be valid [0, size]
    * - You may assume that the data will not be null
@@ -27,7 +27,39 @@ public class SinglyLinkedList<T> {
    * @param data  the data to add
    */
   public void addAtIndex(int index, T data) {
-      // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    SinglyLinkedListNode<T> newNode = new SinglyLinkedListNode<>(data);
+
+    // If index is empty
+    if (index == 0) {
+      newNode.setNext(head);
+      head = newNode;
+
+      if (tail == null) {
+        tail = newNode;
+      }
+    }
+    // Adding to the back
+    else if (index == size) {
+      if (tail != null) {
+        tail.setNext(newNode);
+      } else {
+        head = newNode;
+      }
+      tail = newNode;
+    }
+    // Adding elsewhere
+    else {
+      SinglyLinkedListNode currNode = head;
+      for (int i = 0; i < index - 1; i++) {
+        currNode = currNode.getNext();
+      }
+
+      newNode.setNext(currNode.getNext());
+      currNode.setNext(newNode);
+    }
+
+    size++;
   }
 
   /**
