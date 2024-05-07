658276fc1310504b228624be37ff55e01908d0c7
Mario Fernandez <diddleslip@gmail.com>
2024-05-07 17:11:09 -0400

    Completed selection sort with minimal help from outside resources! (Intentional)

--- III/Module 11/Practice Exam/Selection Sort/Sorting.java
+++ III/Module 11/Practice Exam/Selection Sort/Sorting.java
@@ -21,6 +21,30 @@ public class Sorting {
      * @param comparator The Comparator used to compare the data in arr.
      */
     public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int idx = i;
+
+            for (int e = i - 1; e >= 0; e--) {
+                if (comparator.compare(arr[e], arr[idx]) > 0) {
+                    idx = e;
+                }
+            }
+
+        T temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+        }
     }
 }
+
+/*
+[Executed at: Tue May 7 14:04:29 PDT 2024]
+
+============================================================
+Sorting.java successfully compiled.
+============================================================
+Success: All Tests Passed.
+
+Score: 10.0 / 10.0
+============================================================
+*/
