e7a4405854a2c627e7380db33e21c400b1347447
Mario Fernandez <diddleslip@gmail.com>
2024-05-06 21:24:33 -0400

    mergeSort method checkpoint

--- III/Module 11/Practice Exam/Sorting.java
+++ III/Module 11/Practice Exam/Sorting.java
@@ -5,32 +5,45 @@ import java.util.Comparator;
  */
 public class Sorting {
 
-    /**
-     * Implement merge sort.
-     *
-     * It should be: out-of-place stable not adaptive
-     *
-     * Have a worst case running time of: O(n log n) And a best case running time
-     * of: O(n log n)
-     *
-     * You can create more arrays to run merge sort, but at the end, everything
-     * should be merged back into the original T[] which was passed in.
-     *
-     * When splitting the array, if there is an odd number of elements, put the
-     * extra data on the right side.
-     *
-     * Hint: You may need to create a helper method that merges two arrays back into
-     * the original T[] array. If two data are equal when merging, think about which
-     * subarray you should pull from first.
-     *
-     * You may assume that the passed in array and comparator are both valid and
-     * will not be null.
-     *
-     * @param <T>        Data type to sort.
-     * @param arr        The array to be sorted.
-     * @param comparator The Comparator used to compare the data in arr.
-     */
-    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
-        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+  /**
+   * Implement merge sort.
+   *
+   * It should be: out-of-place stable not adaptive
+   *
+   * Have a worst case running time of: O(n log n) And a best case running time
+   * of: O(n log n)
+   *
+   * You can create more arrays to run merge sort, but at the end, everything
+   * should be merged back into the original T[] which was passed in.
+   *
+   * When splitting the array, if there is an odd number of elements, put the
+   * extra data on the right side.
+   *
+   * Hint: You may need to create a helper method that merges two arrays back into
+   * the original T[] array. If two data are equal when merging, think about which
+   * subarray you should pull from first.
+   *
+   * You may assume that the passed in array and comparator are both valid and
+   * will not be null.
+   *
+   * @param <T>        Data type to sort.
+   * @param arr        The array to be sorted.
+   * @param comparator The Comparator used to compare the data in arr.
+   */
+  public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+    if (arr.length == 1) {
+      return;
     }
+
+    int length = arr.length;
+    int midIndex = length / 2;
+    T[] left = (T[]) new Object[midIndex];
+    T[] right = (T[]) new Object[length - midIndex];
+    for (int i = 0; i < midIndex; i++) {
+      left[i] = arr[i];
+    }
+    for (int i = midIndex; i < length; i++) {
+      right[i - midIndex] = arr[i];
+    }
+  }
 }
