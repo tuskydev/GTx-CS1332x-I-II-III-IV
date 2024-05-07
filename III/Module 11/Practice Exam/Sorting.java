eda331f7a8bb09eeac5800974cd081bb98920e71
Mario Fernandez <diddleslip@gmail.com>
2024-05-06 21:16:14 -0400

    Added Practice exam module, file and folder

---
+++ III/Module 11/Practice Exam/Sorting.java
@@ -0,0 +1,36 @@
+import java.util.Comparator;
+
+/**
+ * Your implementation of Merge Sort.
+ */
+public class Sorting {
+
+    /**
+     * Implement merge sort.
+     *
+     * It should be: out-of-place stable not adaptive
+     *
+     * Have a worst case running time of: O(n log n) And a best case running time
+     * of: O(n log n)
+     *
+     * You can create more arrays to run merge sort, but at the end, everything
+     * should be merged back into the original T[] which was passed in.
+     *
+     * When splitting the array, if there is an odd number of elements, put the
+     * extra data on the right side.
+     *
+     * Hint: You may need to create a helper method that merges two arrays back into
+     * the original T[] array. If two data are equal when merging, think about which
+     * subarray you should pull from first.
+     *
+     * You may assume that the passed in array and comparator are both valid and
+     * will not be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array to be sorted.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void mergeSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+}
