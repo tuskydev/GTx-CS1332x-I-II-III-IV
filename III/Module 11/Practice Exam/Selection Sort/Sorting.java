76e32dcd02e6befe852dd055bab41c5f399d8925
Mario Fernandez <diddleslip@gmail.com>
2024-05-07 16:27:14 -0400

    Added Selection sort folder and file!

---
+++ III/Module 11/Practice Exam/Selection Sort/Sorting.java
@@ -0,0 +1,26 @@
+import java.util.Comparator;
+
+/**
+ * Your implementation of Selection Sort.
+ */
+public class Sorting {
+
+    /**
+     * Implement selection sort.
+     *
+     * It should be: in-place unstable not adaptive
+     *
+     * Have a worst case running time of: O(n^2) And a best case running time of:
+     * O(n^2)
+     *
+     * You may assume that the passed in array and comparator are both valid and
+     * will never be null.
+     *
+     * @param <T>        Data type to sort.
+     * @param arr        The array that must be sorted after the method runs.
+     * @param comparator The Comparator used to compare the data in arr.
+     */
+    public static <T> void selectionSort(T[] arr, Comparator<T> comparator) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+}
